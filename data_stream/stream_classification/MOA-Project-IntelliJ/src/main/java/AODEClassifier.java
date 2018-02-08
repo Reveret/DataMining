88326dcd22bc572f355e6e247d6c4ae989593973
Roc <rgranada@uni-mainz.de>
2018-02-08 12:57:20 +0100

    stream class pdf started

--- data_stream/stream_classification/MOA-Project-IntelliJ/src/main/java/AODEClassifier.java
+++ data_stream/stream_classification/MOA-Project-IntelliJ/src/main/java/AODEClassifier.java
@@ -1,10 +1,18 @@
 
+import moa.core.TimingUtils;
 import weka.classifiers.Classifier;
 import moa.evaluation.LearningCurve;
 import moa.evaluation.WindowClassificationPerformanceEvaluator;
 import moa.streams.ArffFileStream;
 import moa.tasks.EvaluatePrequential;
 import weka.classifiers.bayes.AODE;
+import weka.classifiers.meta.MOA;
+import weka.core.Instance;
+import weka.core.Instances;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class AODEClassifier {
 
@@ -14,7 +22,7 @@ public class AODEClassifier {
         this.file = file;
     }
 
-    public void run(int numInstances) {
+    public void run(int numInstances) throws Exception{
         Classifier learner = new AODE();
 
         ArffFileStream stream = new ArffFileStream(file,-1);
@@ -38,6 +46,39 @@ public class AODEClassifier {
         LearningCurve le = (LearningCurve) ep.doTask();
         System.out.println("\nEvaluate prequential using AODE");
         System.out.println(le);
+
+        /*
+        TRIED TO USE THE MOA API CODE TO ADAPT THE WEKA CLASSIFIER TO PREQUENTIAL EVALUATION BUT
+        IT DID NOT WORK
+
+        Classifier learner = new AODE();
+        BufferedReader reader = new BufferedReader(
+                new FileReader("/some/where/data.arff"));
+        Instances stream = new Instances(reader);
+        reader.close();
+        // setting class attribute
+        stream.setClassIndex(stream.numAttributes() - 1);
+
+
+        learner.buildClassifier(stream.firstInstance());
+
+        int numberSamplesCorrect = 0;
+        int numberSamples = 0;
+        boolean preciseCPUTiming = TimingUtils.enablePreciseTiming();
+        long evaluateStartTime = TimingUtils.getNanoCPUTimeOfCurrentThread();
+        while (stream.hasMoreInstances() && numberSamples < numInstances) {
+            Instance trainInst = stream.nextInstance().getData();
+            if (isTesting) {
+                if (learner.correctlyClassifies(trainInst)){
+                    numberSamplesCorrect++;
+                }
+            }
+            numberSamples++;
+            learner.trainOnInstance(trainInst);
+        }
+        double accuracy = 100.0 * (double) numberSamplesCorrect/ (double) numberSamples;
+        double time = TimingUtils.nanoTimeToSeconds(TimingUtils.getNanoCPUTimeOfCurrentThread()- evaluateStartTime);
+        System.out.println(numberSamples + " instances processed with " + accuracy + "% accuracy in "+time+" seconds.");*/
     }
 
 }
